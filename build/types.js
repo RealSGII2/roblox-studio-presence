4ff938b13121d03c08e20da103deac85f39bdf09
Thomas Wise <thomas@realsgii2.dev>
2021-11-29 16:45:42 -0600

    Delete build directory

--- build/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ActivityToken = void 0;
-var ActivityToken;
-(function (ActivityToken) {
-    ActivityToken[ActivityToken["Place"] = 0] = "Place";
-    ActivityToken[ActivityToken["Script"] = 1] = "Script";
-})(ActivityToken = exports.ActivityToken || (exports.ActivityToken = {}));
